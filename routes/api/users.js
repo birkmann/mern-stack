50a48e04d52b13630eed66a88f9fcc383d511e6d
Rafael Birkmann <mail@rafaelbirkmann.de>
2020-03-11 22:18:26 +0100

    files

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
-router.get("/", (req, res) => res.send("User route"));
+router.post(
+  "/",
+  [
+    check("name", "Name ist required")
+      .not()
+      .isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password has to be at least 6 characters").isLength({
+      min: 6
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("User route");
+  }
+);
 
 module.exports = router;
