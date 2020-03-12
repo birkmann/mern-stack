f45969fe113ad88ab6047b44768d1eb1fc23af97
Rafael Birkmann <mail@rafaelbirkmann.de>
2020-03-12 11:59:15 +0100

    token auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-router.get("/", (req, res) => res.send("Auth route"));
+router.get("/", auth, (req, res) => res.send("Auth route"));
 
 module.exports = router;
