493045669a31c7d11da265c6393c841582563564
Rafael Birkmann <mail@rafaelbirkmann.de>
2020-03-12 13:42:52 +0100

    files

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,47 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
-router.get("/", (req, res) => res.send("Profile route"));
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id
+    });
+
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ msg: "There ist no profile for this user" });
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required")
+        .not()
+        .isEmpty(),
+      check("skills", "Skills is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 
 module.exports = router;
