172c8956969a9a5cef21a74a5563ca32e2cac789
Rafael Birkmann <mail@rafaelbirkmann.de>
2020-03-12 01:21:29 +0100

    registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
-const bcrypt = require("bcrypt.js");
+const bcrypt = require("bcryptjs");
 
 const { check, validationResult } = require("express-validator");
 
